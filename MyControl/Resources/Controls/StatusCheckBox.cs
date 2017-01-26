9950ac41ddfa583ebfc4dae5a283c67327eefc76
huangjia2107 <huangjia2107@users.noreply.github.com>
2017-01-26 11:14:10 +0800

    Update StatusCheckBox.cs

--- MyControl/Resources/Controls/StatusCheckBox.cs
+++ MyControl/Resources/Controls/StatusCheckBox.cs
@@ -6,7 +6,7 @@ using System.Windows.Controls;
 using System.Windows;
 using System.ComponentModel;
 
-namespace AmicaUI.Resources.Controls
+namespace MyControl.Resources.Controls
 {
     class StatusCheckBox : CheckBox
     {
